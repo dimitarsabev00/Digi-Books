ef25187735a1c39a694c37fdf45f3c3d979f3a0b
Dimitar <wopz1337@gmail.com>
2022-12-15 23:00:51 +0200

    Added Login & Register Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import AuthLayout from "./components/AuthLayout/AuthLayout";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          <AuthLayout>
+            <Login />
+          </AuthLayout>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <AuthLayout>
+            <Register />
+          </AuthLayout>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
