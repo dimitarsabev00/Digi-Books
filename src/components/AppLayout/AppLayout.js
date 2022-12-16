06e62cd3f327b375c310dd5c86b38f1992bad904
Dimitar <wopz1337@gmail.com>
2022-12-16 20:22:11 +0200

    Added Back Button In AppLayout Component

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -1,20 +1,30 @@
 import "./AppLayout.scss";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { ReactComponent as ProfileIcon } from "../../assets/profile.svg";
 import CustomLink from "../CustomLink/CustomLink";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/slices/AuthSlice";
+import { IoCaretBack } from "react-icons/io5";
 
-function AppLayout() {
+function AppLayout({ detail = false }) {
   const dispatch = useDispatch();
   return (
     <div id="page">
       <div id="nav-background">
         <div className="container">
           <div id="navbar">
-            <Logo id="logo" />
+            {detail ? (
+              <Link to="/home">
+                <div className="back-btn">
+                  <IoCaretBack size={23} />
+                  <span>Library</span>
+                </div>
+              </Link>
+            ) : (
+              <Logo id="logo" />
+            )}
 
             <nav id="links">
               <CustomLink to={"/home"}>Library</CustomLink>
