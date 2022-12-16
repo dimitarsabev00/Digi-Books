22ca9a90335ce7f707bfc22759ebf804e59689ee
Dimitar <wopz1337@gmail.com>
2022-12-16 22:02:37 +0200

    Added Search Book Functionality

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import "./HomePage.scss";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
-import { useEffect } from "react";
-import { getAllBooks } from "../../store/slices/BooksSlice";
+import { useEffect, useState } from "react";
+import { getAllBooks, searchBooks } from "../../store/slices/BooksSlice";
 import { useDispatch, useSelector } from "react-redux";
 import BookCard from "../BookCard/BookCard";
 function HomePage() {
+  const [pattern, setPattern] = useState("");
+  const [searchLabel, setSearchLabel] = useState("All BOOKS");
   const dispatch = useDispatch();
   const { booksList, booksIsLoading } = useSelector(
     ({ booksState }) => booksState
@@ -15,18 +17,32 @@ function HomePage() {
     dispatch(getAllBooks());
   }, []);
 
+  useEffect(() => {
+    if (pattern) {
+      dispatch(searchBooks(pattern));
+    } else {
+      dispatch(getAllBooks());
+    }
+  }, [pattern]);
   return (
     <div className="outlet-container">
       <div className="content-container">
         <div id="search-form">
-          <label htmlFor="search">ALL BOOKS</label>
+          <label htmlFor="search">{searchLabel}</label>
           <div>
-            <input type="text" id="search" placeholder="Search" />
+            <input
+              type="text"
+              id="search"
+              placeholder="Search"
+              value={pattern}
+              onChange={(ev) => setPattern(ev.target.value)}
+            />
             <SearchIcon id="search-icon" />
           </div>
         </div>
+
         <div id="book-grid">
-          {booksList.map((book) => (
+          {booksList?.map((book) => (
             <BookCard key={book._id} {...book} />
           ))}
         </div>
