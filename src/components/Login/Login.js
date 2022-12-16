33f209cf6f29d8dfea0fae79d2314b171f68c049
Dimitar <wopz1337@gmail.com>
2022-12-16 16:21:30 +0200

    Added Login Functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,49 @@
+import { useState } from "react";
 import "./Login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoMdEye } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { signIn } from "../../store/slices/AuthSlice";
 
 function LoginForm() {
+  const [password, setPassword] = useState({ isVisable: false });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const loginIsLoading = useSelector(
+    ({ authState: { loginIsLoading } }) => loginIsLoading
+  );
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const username = e.target[0].value;
+    const password = e.target[1].value;
+    dispatch(signIn({ username, password }, navigate));
+  };
   return (
     <div id="login-form">
       <h1>WELCOME BACK!</h1>
-      <form>
+      <form onSubmit={handleLogin}>
         <div className="form-element">
-          <label htmlFor="username">Email</label>
+          <label htmlFor="username">Username</label>
           <input type="text" id="username" />
         </div>
         <div className="form-element">
           <label htmlFor="password">Password</label>
           <div className="password-field">
-            <input type="password" id="password" />
-            <IoMdEye id="eye-icon" />
+            <input
+              type={password.isVisable ? "input" : "password"}
+              id="password"
+            />
+            <IoMdEye
+              id="eye-icon"
+              className={password.isVisable ? "active" : ""}
+              onClick={() =>
+                setPassword((prev) => ({
+                  ...prev,
+                  isVisable: !prev.isVisable,
+                }))
+              }
+            />
           </div>
         </div>
         <a id="recover" href="#">
