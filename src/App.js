b72aaad35f96f953c1b28031ff363e73532138fc
Dimitar <wopz1337@gmail.com>
2022-12-16 08:56:02 +0200

    Import & Added AppLayout & HomePage In Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./components/AppLayout/AppLayout";
 import AuthLayout from "./components/AuthLayout/AuthLayout";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to={"home"} replace />} />
       <Route
         path="/login"
         element={
@@ -23,6 +26,11 @@ function App() {
           </AuthLayout>
         }
       />
+      <Route path={"/home"} element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+
+      <Route path="*" element={<div>There's no page here</div>} />
     </Routes>
   );
 }
