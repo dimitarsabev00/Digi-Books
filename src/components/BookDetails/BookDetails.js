e8a6225364d281ade34abde23751e061aea40630
Dimitar <wopz1337@gmail.com>
2022-12-17 00:25:30 +0200

    Added Spinner In BookDetails Component

--- src/components/BookDetails/BookDetails.js
+++ src/components/BookDetails/BookDetails.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import "./BookDetails.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { getBookDetails } from "../../store/slices/BooksSlice";
+import { TailSpin } from "react-loader-spinner";
 
 function BookDetails() {
   let { bookId } = useParams();
@@ -21,60 +22,86 @@ function BookDetails() {
     return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
   }
   return (
-    <div className="details">
-      <div className="image-container">
-        <img src={singleBook?.image} alt={singleBook?.name} />
-      </div>
-
-      <div className="book-data">
-        <div className="title-container">
-          <h1>{singleBook?.name}</h1>
-        </div>
-
-        <div className="body-details">
-          <div className="general-info">
-            <p className="author-details">{singleBook?.author}</p>
-            <p>
-              Genre:{" "}
-              <span className="span-details">{singleBook?.genre?.name}</span>
-            </p>
-
-            <p>
-              Created on:{" "}
-              <span className="span-details">
-                {formatDate(singleBook?.createOn)}
-              </span>
-            </p>
-            <p>
-              Updated on:{" "}
-              <span className="span-details">
-                {formatDate(singleBook?.lastUpdateOn)}
-              </span>
-            </p>
+    <>
+      {!singleBooksLoading ? (
+        <div className="details">
+          <div className="image-container">
+            <img src={singleBook?.image} alt={singleBook?.name} />
           </div>
-          <div className="description">
-            <h3>Short description</h3>
-            <p>
-              Harry Potter and the Goblet of Fire is a fantasy novel written by
-              British author J. K. Rowling and the fourth novel in the Harry
-              Potter series. It follows Harry Potter, a wizard in his fourth
-              year at Hogwarts School of Witchcraft and Wizardry, and the
-              mystery surrounding the entry of Harry’s name into the Triwizard
-              Tournament, in which he is forced to compete.
-            </p>
-            <p>
-              The book was published in the United Kingdom by Bloomsbury and in
-              the United States by Scholastic. In both countries, the release
-              date was 8 July 2000. This was the first time a book in the series
-              was published in both countries at the same time. The novel won a
-              Hugo Award, the only Harry Potter novel to do so, in 2001. The
-              book was adapted into a film, released worldwide on 18 November
-              2005, and a video game by Electronic Arts.
-            </p>
+
+          <div className="book-data">
+            <div className="title-container">
+              <h1>{singleBook?.name}</h1>
+            </div>
+
+            <div className="body-details">
+              <div className="general-info">
+                <p className="author-details">{singleBook?.author}</p>
+                <p>
+                  Genre:{" "}
+                  <span className="span-details">
+                    {singleBook?.genre?.name}
+                  </span>
+                </p>
+
+                <p>
+                  Created on:{" "}
+                  <span className="span-details">
+                    {formatDate(singleBook?.createOn)}
+                  </span>
+                </p>
+                <p>
+                  Updated on:{" "}
+                  <span className="span-details">
+                    {formatDate(singleBook?.lastUpdateOn)}
+                  </span>
+                </p>
+              </div>
+              <div className="description">
+                <h3>Short description</h3>
+                <p>
+                  Harry Potter and the Goblet of Fire is a fantasy novel written
+                  by British author J. K. Rowling and the fourth novel in the
+                  Harry Potter series. It follows Harry Potter, a wizard in his
+                  fourth year at Hogwarts School of Witchcraft and Wizardry, and
+                  the mystery surrounding the entry of Harry’s name into the
+                  Triwizard Tournament, in which he is forced to compete.
+                </p>
+                <p>
+                  The book was published in the United Kingdom by Bloomsbury and
+                  in the United States by Scholastic. In both countries, the
+                  release date was 8 July 2000. This was the first time a book
+                  in the series was published in both countries at the same
+                  time. The novel won a Hugo Award, the only Harry Potter novel
+                  to do so, in 2001. The book was adapted into a film, released
+                  worldwide on 18 November 2005, and a video game by Electronic
+                  Arts.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "3rem",
+          }}
+        >
+          <TailSpin
+            height="80"
+            width="80"
+            color="#08c642"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
