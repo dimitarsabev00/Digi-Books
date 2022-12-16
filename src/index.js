876b315764b2f16b90b6a5e4c92eb559e516fe6f
Dimitar <wopz1337@gmail.com>
2022-12-16 19:01:18 +0200

    Added ToastContainer In index.js File

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { Provider as ReduxProvider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { store } from "./store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <ToastContainer autoClose={5000} position="bottom-center" />
     <ReduxProvider store={store}>
       <Router>
         <App />
