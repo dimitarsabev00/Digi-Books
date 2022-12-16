7055523ca380513c826b80ac7acef8c1cb8827b0
Dimitar <wopz1337@gmail.com>
2022-12-16 16:02:32 +0200

    Added AuthSlice

---
+++ src/store/slices/AuthSlice.js
@@ -0,0 +1,103 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import axiosHelper from "../../api/axiosHelper";
+
+const localStorageToken = localStorage.getItem("token") ?? "";
+
+const initialState = {
+  isAuthenticated: Boolean(localStorageToken),
+  token: localStorageToken,
+  loginIsLoading: false,
+  registerIsLoading: false,
+};
+
+export const AuthSlice = createSlice({
+  name: "Authentication",
+  initialState,
+  reducers: {
+    loginRequesting: (state) => {
+      state.loginIsLoading = true;
+    },
+    loginSuccess: (state, { payload }) => {
+      const token = payload?.data?.token;
+      localStorage.setItem("token", token);
+      state.isAuthenticated = true;
+      state.loginIsLoading = false;
+      toast.success("You have been logged in successfully!");
+    },
+    loginError: (state) => {
+      state.loginIsLoading = false;
+    },
+    registerRequesting: (state) => {
+      state.registerIsLoading = true;
+    },
+    registerSuccess: (state, { payload }) => {
+      state.registerIsLoading = false;
+
+      const successMessage = payload?.data?.message;
+
+      if (successMessage) {
+        toast.success(successMessage);
+      }
+    },
+    registerError: (state) => {
+      state.registerIsLoading = false;
+    },
+    logoutSuccess: (state, { payload }) => {
+      state.isAuthenticated = false;
+      state.token = "";
+      localStorage.removeItem("token");
+
+      const successMessage = payload?.data?.message;
+
+      if (successMessage) {
+        toast.success(successMessage);
+      }
+    },
+  },
+});
+
+export const {
+  loginRequesting,
+  loginSuccess,
+  loginError,
+  registerRequesting,
+  registerSuccess,
+  registerError,
+  logoutSuccess,
+} = AuthSlice.actions;
+
+export const signIn = (data, navigate) => async (dispatch) => {
+  dispatch(loginRequesting());
+  axiosHelper.post("/user/login", data).then(
+    (response) => {
+      dispatch(loginSuccess(response));
+      if (navigate) {
+        navigate("/");
+      }
+    },
+
+    (error) => dispatch(loginError(error))
+  );
+};
+
+export const signUp = (data, navigate) => async (dispatch) => {
+  dispatch(registerRequesting());
+  axiosHelper.post("/user/register", data).then(
+    (response) => {
+      dispatch(registerSuccess(response));
+      if (navigate) {
+        navigate("/login");
+      }
+    },
+    (error) => dispatch(registerError(error))
+  );
+};
+
+export const logout = () => async (dispatch) => {
+  axiosHelper
+    .post("/user/logout")
+    .then((response) => dispatch(logoutSuccess(response)));
+};
+
+export default AuthSlice.reducer;
