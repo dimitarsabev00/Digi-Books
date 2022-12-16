bd8b674807ba365f7cadd8e5bbcc47915f27c728
Dimitar <wopz1337@gmail.com>
2022-12-16 18:31:28 +0200

    Added BooksSlice In Redux Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./slices/AuthSlice";
+import BooksReducer from "./slices/BooksSlice";
 
 export const store = configureStore({
   reducer: {
     authState: AuthReducer,
+    booksState: BooksReducer,
   },
 });
