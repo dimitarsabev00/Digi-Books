625c2bfbd0cab4a172771439067ae41974f93bd1
Dimitar <wopz1337@gmail.com>
2022-12-17 15:29:48 +0200

    Added Responsive Design For All Components

--- src/components/AppLayout/AppLayout.js
+++ src/components/AppLayout/AppLayout.js
@@ -6,26 +6,61 @@ import { ReactComponent as ProfileIcon } from "../../assets/profile.svg";
 import CustomLink from "../CustomLink/CustomLink";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/slices/AuthSlice";
-import { IoCaretBack } from "react-icons/io5";
+import { IoCaretBack, IoCloseOutline, IoMenuOutline } from "react-icons/io5";
+import { useEffect, useState } from "react";
 
 function AppLayout({ detail = false }) {
+  const [hamMenu, sethamMenu] = useState(false);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const dispatch = useDispatch();
+
+  //close dropdown on resize
+  useEffect(() => {
+    const handleResize = () => {
+      setInnerWidth(window.innerWidth);
+
+      if (innerWidth > 800) {
+        sethamMenu(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [innerWidth]);
+
   return (
     <div id="page">
       <div id="nav-background">
         <div className="container">
           <div id="navbar">
-            {detail ? (
-              <Link to="/home">
-                <div className="back-btn">
-                  <IoCaretBack size={23} />
-                  <span>Library</span>
-                </div>
-              </Link>
+            {hamMenu ? (
+              <IoCloseOutline
+                className="ham-menu-icon"
+                onClick={() => sethamMenu((prev) => !prev)}
+              />
             ) : (
-              <Logo id="logo" />
+              <IoMenuOutline
+                className="ham-menu-icon"
+                onClick={() => sethamMenu((prev) => !prev)}
+              />
             )}
-
+            {detail && innerWidth > 800 && (
+              <>
+                <Link to="/home">
+                  <div className="back-btn">
+                    <IoCaretBack size={23} />
+                    <span>Library</span>
+                  </div>
+                </Link>
+              </>
+            )}
+            {!detail && innerWidth > 800 && <Logo id="logo" />}
+            {innerWidth < 800 ? <Logo id="logo" /> : null}
             <nav id="links">
               <CustomLink to={"/home"}>Library</CustomLink>
 
@@ -36,7 +71,22 @@ function AppLayout({ detail = false }) {
             </div>
           </div>
         </div>
+        {hamMenu && (
+          <nav id="links-ham">
+            <CustomLink to={"/home"}>Library</CustomLink>
+
+            <CustomLink to={"/home/settings"}>Settings</CustomLink>
+          </nav>
+        )}
       </div>
+      {detail && innerWidth < 800 && (
+        <Link to="/home" style={{ textDecoration: "none" }}>
+          <div className="back-btn">
+            <IoCaretBack size={23} />
+            <span>Library</span>
+          </div>
+        </Link>
+      )}
       <Outlet style={{ "z-index": 5 }} />
     </div>
   );
